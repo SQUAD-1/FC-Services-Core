defb3f144a95120bc55e05377cf162398ff57ce7
alisson.assuncao-at-171944429159 <alisson.assuncao@ferreiracosta.com.br>
2023-05-17 20:46:19 -0300

    .

--- Controllers/FiltrarChamado.cs
+++ Controllers/FiltrarChamado.cs
@@ -1,98 +1,98 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using Microsoft.AspNetCore.Authorization;
+// using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using Microsoft.AspNetCore.Mvc;
+// using MySql.Data.MySqlClient;
+// using Microsoft.AspNetCore.Authorization;
 
-namespace FC_Services_Core.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class FiltrarChamadosController : ControllerBase
-    {
-        [HttpPost]
-        [Authorize]
-        public IActionResult FiltrarChamados(FiltroChamados filtro)
-        {
-            string connectionString = "server=containers-us-west-209.railway.app;port=6938;database=railway;user=root;password=5cu1Y8DVEYLMeej8yleH";
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            MySqlCommand command = connection.CreateCommand();
+// namespace FC_Services_Core.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class FiltrarChamadosController : ControllerBase
+//     {
+//         [HttpPost]
+//         [Authorize]
+//         public IActionResult FiltrarChamados(FiltroChamados filtro)
+//         {
+//             string connectionString = "server=containers-us-west-209.railway.app;port=6938;database=railway;user=root;password=5cu1Y8DVEYLMeej8yleH";
+//             MySqlConnection connection = new MySqlConnection(connectionString);
+//             MySqlCommand command = connection.CreateCommand();
 
-            string query = "SELECT * FROM Chamado WHERE ";
+//             string query = "SELECT * FROM Chamado WHERE ";
 
-            List<MySqlParameter> parameters = new List<MySqlParameter>();
+//             List<MySqlParameter> parameters = new List<MySqlParameter>();
 
-            if (filtro.DataRelato != null)
-            {
-                query += "DataRelato = @DataRelato AND ";
-                parameters.Add(new MySqlParameter("@DataRelato", filtro.DataRelato));
-            }
+//             if (filtro.DataRelato != null)
+//             {
+//                 query += "DataRelato = @DataRelato AND ";
+//                 parameters.Add(new MySqlParameter("@DataRelato", filtro.DataRelato));
+//             }
 
-            if (filtro.Prioridade != null)
-            {
-                query += "Prioridade = @Prioridade AND ";
-                parameters.Add(new MySqlParameter("@Prioridade", filtro.Prioridade));
-            }
+//             if (filtro.Prioridade != null)
+//             {
+//                 query += "Prioridade = @Prioridade AND ";
+//                 parameters.Add(new MySqlParameter("@Prioridade", filtro.Prioridade));
+//             }
 
-            if (filtro.Status != null)
-            {
-                query += "Status = @Status AND ";
-                parameters.Add(new MySqlParameter("@Status", filtro.Status));
-            }
+//             if (filtro.Status != null)
+//             {
+//                 query += "Status = @Status AND ";
+//                 parameters.Add(new MySqlParameter("@Status", filtro.Status));
+//             }
 
-            if (filtro.TempoDecorrido != null)
-            {
-                query += "TempoDecorrido = @TempoDecorrido AND ";
-                parameters.Add(new MySqlParameter("@TempoDecorrido", filtro.TempoDecorrido));
-            }
+//             if (filtro.TempoDecorrido != null)
+//             {
+//                 query += "TempoDecorrido = @TempoDecorrido AND ";
+//                 parameters.Add(new MySqlParameter("@TempoDecorrido", filtro.TempoDecorrido));
+//             }
 
-            if (filtro.Empregado_Matricula != null)
-            {
-                query += "Empregado_Matricula = @Empregado_Matricula AND ";
-                parameters.Add(new MySqlParameter("@Empregado_Matricula", filtro.Empregado_Matricula));
-            }
+//             if (filtro.Empregado_Matricula != null)
+//             {
+//                 query += "Empregado_Matricula = @Empregado_Matricula AND ";
+//                 parameters.Add(new MySqlParameter("@Empregado_Matricula", filtro.Empregado_Matricula));
+//             }
 
-            if (filtro.Tipo != null)
-            {
-                query += "Tipo = @Tipo AND ";
-                parameters.Add(new MySqlParameter("@Tipo", filtro.Tipo));
-            }
+//             if (filtro.Tipo != null)
+//             {
+//                 query += "Tipo = @Tipo AND ";
+//                 parameters.Add(new MySqlParameter("@Tipo", filtro.Tipo));
+//             }
 
-            // Remove o último "AND" da consulta
-            query = query.TrimEnd(' ', 'A', 'N', 'D');
+//             // Remove o último "AND" da consulta
+//             query = query.TrimEnd(' ', 'A', 'N', 'D');
 
-            command.CommandText = query;
-            command.Parameters.AddRange(parameters.ToArray());
+//             command.CommandText = query;
+//             command.Parameters.AddRange(parameters.ToArray());
 
-            connection.Open();
+//             connection.Open();
 
-            MySqlDataReader reader = command.ExecuteReader();
+//             MySqlDataReader reader = command.ExecuteReader();
 
-            List<Chamado> chamados = new List<Chamado>();
+//             List<Chamado> chamados = new List<Chamado>();
 
-            while (reader.Read())
-            {
-                Chamado chamado = new Chamado
-                {
-                    Nome = reader["Nome"].ToString(),
-                    DataRelato = reader["DataRelato"].ToString(),
-                    Descricao = reader["Descricao"].ToString(),
-                    Prioridade = reader["Prioridade"].ToString(),
-                    HorarioAbertura = reader["HorarioAbertura"].ToString(),
-                    HorarioUltimaAtualizacao = reader["HorarioUltimaAtualizacao"].ToString(),
-                    Status = reader["Status"].ToString(),
-                    TempoDecorrido = reader["TempoDecorrido"].ToString(),
-                    Empregado_Matricula = Convert.ToInt32(reader["Empregado_Matricula"]),
-                    Tipo = reader["Tipo"].ToString()
-                };
+//             while (reader.Read())
+//             {
+//                 Chamado chamado = new Chamado
+//                 {
+//                     Nome = reader["Nome"].ToString(),
+//                     DataRelato = reader["DataRelato"].ToString(),
+//                     Descricao = reader["Descricao"].ToString(),
+//                     Prioridade = reader["Prioridade"].ToString(),
+//                     HorarioAbertura = reader["HorarioAbertura"].ToString(),
+//                     HorarioUltimaAtualizacao = reader["HorarioUltimaAtualizacao"].ToString(),
+//                     Status = reader["Status"].ToString(),
+//                     TempoDecorrido = reader["TempoDecorrido"].ToString(),
+//                     Empregado_Matricula = Convert.ToInt32(reader["Empregado_Matricula"]),
+//                     Tipo = reader["Tipo"].ToString()
+//                 };
 
-                chamados.Add(chamado);
-            }
+//                 chamados.Add(chamado);
+//             }
 
-            connection.Close();
+//             connection.Close();
 
-            return Ok(chamados);
-        }
-    }
-}
+//             return Ok(chamados);
+//         }
+//     }
+// }
