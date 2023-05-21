bbf54f44922a26b1ca1e627f2a299e9940f884d9
Marcos José <marcosjos15@hotmail.com>
2023-05-21 17:10:07 -0300

    MatriculaComentario

--- Controllers/AddComentario.cs
+++ Controllers/AddComentario.cs
@@ -12,17 +12,18 @@ namespace backend_squad1.Controllers
     [Route("[controller]")]
     public class AddComentarioController : ControllerBase
     {
-        [HttpPost("adicionar-comentario/{idChamado}")]
+        [HttpPost("adicionar-comentario/{idChamado}/{matricula}")]
         [Authorize]
-        public IActionResult AdicionarComentario(int idChamado, Comentario comentario)
+        public IActionResult AdicionarComentario(int idChamado, int matricula, Comentario comentario)
         {
             string connectionString = "server=gateway01.us-east-1.prod.aws.tidbcloud.com;port=4000;database=mydb;user=2yztCux73sSBMGV.root;password=A857G3OyIUoJOifl";
             using MySqlConnection connection = new MySqlConnection(connectionString);
             using MySqlCommand command = connection.CreateCommand();
 
-            command.CommandText = "INSERT INTO Comentario (IdChamado, Texto, DataComentario) VALUES (@IdChamado, @Texto, @DataComentario); SELECT LAST_INSERT_ID();";
+            command.CommandText = "INSERT INTO Comentario (IdChamado, matricula, Texto, DataComentario) VALUES (@IdChamado, @matricula, @Texto, @DataComentario); SELECT LAST_INSERT_ID();";
 
             command.Parameters.AddWithValue("@IdChamado", idChamado);
+            command.Parameters.AddWithValue("@matricula", matricula);
             command.Parameters.AddWithValue("@Texto", comentario.Texto);
             command.Parameters.AddWithValue("@DataComentario", DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
 
