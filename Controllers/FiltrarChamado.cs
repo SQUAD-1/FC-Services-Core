cf92c1c9758992ac9b620e96b20c87963b56d9d1
Marcos José <marcosjos15@hotmail.com>
2023-06-02 14:01:36 -0300

    Commit

--- Controllers/FiltrarChamado.cs
+++ Controllers/FiltrarChamado.cs
@@ -1,93 +1,93 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-using Microsoft.AspNetCore.Authorization;
-using backend_squad1.Models;
-using backend_squad1.Constants.Sql;
-using backend_squad1.Data;
-using backend_squad1.Models.Inputs;
-using backend_squad1.Data.Models;
-using backend_squad1.Extensions;
+// using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using Microsoft.AspNetCore.Mvc;
+// using MySql.Data.MySqlClient;
+// using Microsoft.AspNetCore.Authorization;
+// using backend_squad1.Models;
+// using backend_squad1.Constants.Sql;
+// using backend_squad1.Data;
+// using backend_squad1.Models.Inputs;
+// using backend_squad1.Data.Models;
+// using backend_squad1.Extensions;
 
-namespace fc_services_core.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class FiltrarChamadosController : ControllerBase
-    {
-        private readonly IAppDbConnection db;
+// namespace fc_services_core.Controllers
+// {
+//     [ApiController]
+//     [Route("[controller]")]
+//     public class FiltrarChamadosController : ControllerBase
+//     {
+//         private readonly IAppDbConnection db;
 
-        public FiltrarChamadosController(IAppDbConnection db)
-        {
-            this.db = db;
-        }
+//         public FiltrarChamadosController(IAppDbConnection db)
+//         {
+//             this.db = db;
+//         }
 
-        [HttpPost]
-        [Authorize]
-        public IActionResult FiltrarChamados(FiltroChamados filtro)
-        {
-            try
-            {
-                this.db.Open();
+//         [HttpPost]
+//         [Authorize]
+//         public IActionResult FiltrarChamados(FiltroChamados filtro)
+//         {
+//             try
+//             {
+//                 this.db.Open();
 
-                string query = "SELECT * FROM chamado WHERE ";
+//                 string query = "SELECT * FROM chamado WHERE ";
 
-                List<MySqlParameter> parameters = new List<MySqlParameter>();
+//                 List<MySqlParameter> parameters = new List<MySqlParameter>();
 
-                if (filtro.DataRelato != null)
-                {
-                    query += "datarelato = @datarelato AND ";
-                    parameters.Add(new MySqlParameter("@datarelato", filtro.DataRelato));
-                }
+//                 if (filtro.DataRelato != null)
+//                 {
+//                     query += "datarelato = @datarelato AND ";
+//                     parameters.Add(new MySqlParameter("@datarelato", filtro.DataRelato));
+//                 }
 
-                if (filtro.Prioridade != null)
-                {
-                    query += "prioridade = @prioridade AND ";
-                    parameters.Add(new MySqlParameter("@prioridade", filtro.Prioridade));
-                }
+//                 if (filtro.Prioridade != null)
+//                 {
+//                     query += "prioridade = @prioridade AND ";
+//                     parameters.Add(new MySqlParameter("@prioridade", filtro.Prioridade));
+//                 }
 
-                if (filtro.Status != null)
-                {
-                    query += "status = @status AND ";
-                    parameters.Add(new MySqlParameter("@status", filtro.Status));
-                }
+//                 if (filtro.Status != null)
+//                 {
+//                     query += "status = @status AND ";
+//                     parameters.Add(new MySqlParameter("@status", filtro.Status));
+//                 }
 
-                if (filtro.TempoDecorrido != null)
-                {
-                    query += "tempodecorrido = @tempodecorrido AND ";
-                    parameters.Add(new MySqlParameter("@tempodecorrido", filtro.TempoDecorrido));
-                }
+//                 if (filtro.TempoDecorrido != null)
+//                 {
+//                     query += "tempodecorrido = @tempodecorrido AND ";
+//                     parameters.Add(new MySqlParameter("@tempodecorrido", filtro.TempoDecorrido));
+//                 }
 
-                if (filtro.Empregado_Matricula != null)
-                {
-                    query += "empregado_matricula = @empregado_matricula AND ";
-                    parameters.Add(new MySqlParameter("@empregado_matricula", filtro.Empregado_Matricula));
-                }
+//                 if (filtro.Empregado_Matricula != null)
+//                 {
+//                     query += "empregado_matricula = @empregado_matricula AND ";
+//                     parameters.Add(new MySqlParameter("@empregado_matricula", filtro.Empregado_Matricula));
+//                 }
 
-                if (filtro.Tipo != null)
-                {
-                    query += "tipo = @tipo AND ";
-                    parameters.Add(new MySqlParameter("@tipo", filtro.Tipo));
-                }
+//                 if (filtro.Tipo != null)
+//                 {
+//                     query += "tipo = @tipo AND ";
+//                     parameters.Add(new MySqlParameter("@tipo", filtro.Tipo));
+//                 }
 
-                // Remove o último "AND" da consulta
-                query = query.TrimEnd(' ', 'A', 'N', 'D');
+//                 // Remove o último "AND" da consulta
+//                 query = query.TrimEnd(' ', 'A', 'N', 'D');
 
-                var chamados = this.db.Query<ConsultaChamadoID>(query, parameters);
+//                 var chamados = this.db.Query<ConsultaChamadoID>(query, parameters);
 
-                return Ok(chamados);
-            }
-            catch (Exception error)
-            {
-                Console.WriteLine(error.Message);
-                return BadRequest();
-            }
-            finally
-            {
-                this.db.Close();
-            }
-        }
-    }
-}
+//                 return Ok(chamados);
+//             }
+//             catch (Exception error)
+//             {
+//                 Console.WriteLine(error.Message);
+//                 return BadRequest();
+//             }
+//             finally
+//             {
+//                 this.db.Close();
+//             }
+//         }
+//     }
+// }
